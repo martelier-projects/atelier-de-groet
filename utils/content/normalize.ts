a2acbb41b68b9b2aed0c0359900751f5aa89aac8
Martijn Nieuwenhuizen <martijnnieuwenhuizen@icloud.com>
2022-01-16 19:04:06 +0100

    Remove log

--- utils/content/normalize.ts
+++ utils/content/normalize.ts
@@ -1,8 +1,6 @@
 // Fix the type of this
 export default (id: string) => (items: any[]) => {
   const allIds = items.map(item => item[id])
-  console.log('allIds: ', allIds)
-
   const byId = items.reduce((collection, item) => {
     return {
       ...collection,
